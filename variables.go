62dd7ffe1ac704ff9ebe6aa4679530fa29dad817
Himanshuxone <himanshusdec8@gmail.com>
2016-10-11 12:30:44 +0530

    declare variables

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	var age int = 26
+	var favnum float64 = 1.6189002
+	randNum := 1
+	fmt.Println(randNum)
+	fmt.Println(favnum)
+	fmt.Println(age)
+}
