6d035e311815eabac3985fddc8234605905174c7
Himanshuxone <himanshusdec8@gmail.com>
2016-10-17 11:55:31 +0530

    append slice

--- slices.go
+++ slices.go
@@ -9,5 +9,6 @@ func main() {
 	b := []string{"George", "Ringo", "Pete"}
 	a = append(a, b...) // equivalent to "append(a, b[0], b[1], b[2])"
 	// a == []string{"John", "Paul", "George", "Ringo", "Pete"}
+	fmt.Println(a)
 
 }
