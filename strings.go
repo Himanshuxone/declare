36f1c55eb9f72acb88787a8e43be52a46ff70d84
Himanshuxone <himanshusdec8@gmail.com>
2016-10-31 22:17:40 +0530

    more on strings

--- strings.go
+++ strings.go
@@ -4,6 +4,7 @@ package main
 import( 
 	"fmt" 
 	"strings" 
+	"sort"
 )
 
 func main() {
@@ -23,5 +24,26 @@ func main() {
  	// replace characters in a string
  	// here replace "l" character with "x" starting with third index
  	fmt.Println(strings.Replace(sampleString, "l", "x", 3))
+
+ 	// create a csv list of values to join and apply more string operations
+ 	csvString := "1,2,3,4,5"
+
+ 	// split the string from comma
+ 	fmt.Println( strings.Split( csvString, "," ) )
+
+ 	// create an array of string type
+
+ 	listOfLetters := []string{"c","b","a"}
+ 	
+ 	// sort the liost of letters
+ 	sort.Strings(listOfLetters)
+ 	fmt.Println(listOfLetters)
+
+ 	// join the letters to using comma
+ 	fmt.Println(strings.Join(listOfLetters, ","))
+
+ 	// create a list of leetrs by joining using comma
+ 	listOfNums := strings.Join([]string{"1","2","3"}, ",")
+ 	fmt.Println(listOfNums)
 
 } 
